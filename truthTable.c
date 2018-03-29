48c106a8e7bd27524855efec4a6fa5717629fc16
sarada <35262650+saradaenrich18@users.noreply.github.com>
2018-03-29 17:01:40 +0530

    Create truthTable.c

---
+++ truthTable.c
@@ -0,0 +1,86 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include<string.h>
+#include<math.h>
+
+int truth_table(int a,int b,char op)
+{
+    switch(op)
+    {
+        case '+':
+            return a|b;
+        case '.':
+            return a&b;
+        case '^':
+            return a^b;
+    }
+}
+
+void printTruthTable(int res[],int a[][100],int row,int col)
+{
+    int i,j;
+    for(i = 0; i < row; i++)
+    {
+        
+        printf("\n");
+        for(j = 0; j < col; j++)
+        {
+            printf("%d ", a[i][j]);
+        }
+        printf("%d  ", res[i]);
+    }
+}
+
+int main() 
+{
+    int a[100][100], res[100], p=0, i, j, k, l=0,count, m = 0;
+    char s[100], operator1[100];
+    scanf("%s", s);
+    
+    for(i = 0; i < strlen(s); i++)
+    {
+        if(s[i] >= 'A' && s[i] <= 'Z')
+        {
+            printf("%c ",s[i]);
+            l++;
+        }
+        else
+        {
+            operator1[m] = s[i];
+            m++;
+        }
+    }
+    printf("%s", s);
+    
+    int powervalue = pow(2,l);
+    for(i = 0; i < l; i++, k = 0, count = 0) 
+    {
+        while(count < pow(2,l))
+        {
+            for(j = 0; j < powervalue/2; j++)
+            {
+                a[k++][i] = p;
+                count++;
+            }
+            for(j = 0; j < powervalue/2; j++)
+            {
+                a[k++][i] = p+1;
+                count++;
+            }
+        }
+        powervalue = powervalue/2;
+    }
+    
+    for(i = 0; i < pow(2,l); i++)
+    {
+        res[i] = truth_table(a[i][0], a[i][1], operator1[0]);
+    }
+    for(int h = 1; h < m; h++)
+    {
+        for(i = 0; i < pow(2,l); i++)
+        {
+            res[i] = truth_table(res[i], a[i][h+1], operator1[h]);
+        }
+    }
+    printTruthTable(res,a,pow(2,l),l);
+}

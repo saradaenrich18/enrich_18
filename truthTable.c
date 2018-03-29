579b6358f19f538fbe290f58a1b273da9bb9e4ac
sarada <35262650+saradaenrich18@users.noreply.github.com>
2018-03-29 20:55:04 +0530

    Update truthTable.c

--- truthTable.c
+++ truthTable.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include<string.h>
 #include<math.h>
-
+int l=0;
 int truth_table(int a,int b,char op)
 {
     switch(op)
@@ -30,16 +30,15 @@ void printTruthTable(int res[],int a[][100],int row,int col)
         printf("%d  ", res[i]);
     }
 }
-
 int main() 
 {
-    int a[100][100], res[100], p=0, i, j, k, l=0,count, m = 0;
+    int a[100][100], res[100], p=0, i, j, k, count, m = 0;
     char s[100], operator1[100];
     scanf("%s", s);
     
     for(i = 0; i < strlen(s); i++)
     {
-        if(s[i] >= 'A' && s[i] <= 'Z')
+        if((s[i] >= 'A' && s[i] <= 'Z')||(s[i] >= 'a' && s[i] <= 'z'))
         {
             printf("%c ",s[i]);
             l++;
@@ -50,27 +49,32 @@ int main()
             m++;
         }
     }
-    printf("%s", s);
-    
-    int powervalue = pow(2,l);
-    for(i = 0; i < l; i++, k = 0, count = 0) 
+    if(l == 0)
     {
+        printf("Enter valid expression");
+        return 0;
+    }
+    printf("%s", s);
+    int power = pow(2,l);
+    for(i = 0; i < l; i++) 
+    {
+        k = 0; 
+        count = 0;
         while(count < pow(2,l))
         {
-            for(j = 0; j < powervalue/2; j++)
+            for(j = 0; j < power/2; j++)
             {
                 a[k++][i] = p;
                 count++;
             }
-            for(j = 0; j < powervalue/2; j++)
+            for(j = 0; j < power/2; j++)
             {
                 a[k++][i] = p+1;
                 count++;
             }
         }
-        powervalue = powervalue/2;
+        power = power/2;
     }
-    
     for(i = 0; i < pow(2,l); i++)
     {
         res[i] = truth_table(a[i][0], a[i][1], operator1[0]);
